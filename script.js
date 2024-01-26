cb66fc125607ba9a19c33e9af6c8a76056831502
sobermind0410 <149725415+sobermind0410@users.noreply.github.com>
2024-01-26 14:34:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,37 @@ function showAlert(massage, className) {
     setTimeout(() => document.querySelector(".alert").remove(), 3000);
 }
 
+//clear all fields 
+function clearFields() {
+    document.querySelector("#firstName").value = "";
+    document.querySelector("#lastName").value = "";
+    document.querySelector("#rollNo").value = "";
+}
+
+// Add Data
+
+document.querySelector("#student-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // get form values
+    const firstName = document.querySelecto("#firstName").value;
+    const lastName = document.querySelecto("#lastName").value;
+    const rollNo = document.querySelecto("#rollNo").value;
+
+    if(firstName == "" || lastName == "" || rollNo == "") {
+        showAlert("Please fill in all fields!", "danger");
+    }
+    else if(selectedRow == null) {
+        const list = document.querySelector("#student-list");
+        const row = document.createElement("tr");
+
+        row. innerHTML = `
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${rollNo}</td>
+    }
+});
+
 // del data 
 document.querySelector("#student-list").addEventListener("click", (e) => {
     target = e.target;
